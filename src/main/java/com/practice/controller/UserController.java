6683624d1e0a0f541fc0f1c86b3df9085826afdb
Suya <suyakozh@gmail.com>
2022-03-26 14:11:12 -0500

    Converter, New user "Save"

--- src/main/java/com/practice/controller/UserController.java
+++ src/main/java/com/practice/controller/UserController.java
@@ -1,21 +1,47 @@
 package com.practice.controller;
 
 import com.practice.dto.UserDTO;
+import com.practice.service.RoleService;
+import com.practice.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
 
         return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
+
+        //model.addAttribute("user", new UserDTO());  NO NEED when redirecting
+        //model.addAttribute("roles", roleService.findAll());  NO NEED when redirecting
+
+        userService.save(user);
+       // model.addAttribute("users", userService.findAll());   NO NEED when redirecting
+
+        return "redirect:/user/create";
     }
 }
---
+++ src/main/java/com/practice/converter/RoleDtoConverter.java
@@ -0,0 +1,24 @@
+package com.practice.converter;
+
+import com.practice.dto.RoleDTO;
+import com.practice.service.RoleService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
+
+    RoleService roleService;
+
+    //injection
+    public RoleDtoConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleDTO convert(String source) {
+        return roleService.findById(Long.parseLong(source));
+    }
+}
